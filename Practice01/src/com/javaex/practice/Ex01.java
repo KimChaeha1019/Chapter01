b6acc3ad6bf66a7e160f973a96eea2f71d98784d
kimchija119@gmail.com <kimchija119@gmail.com>
2025-04-17 12:31:13 +0900

    practice01

---
+++ Practice01/src/com/javaex/practice/Ex01.java
@@ -0,0 +1,22 @@
+package com.javaex.practice;
+
+public class Ex01 {
+	
+	public static void main(String[] args) {
+		
+		/*
+		다음중 기본 자료형중 정수형자료형이 아닌 것은?
+		(1)	int
+		(2)	float
+		(3)	byte
+		(4)	long
+		
+		정답 : (2)float
+			float는 정수형자로형이 아닌 실수형자료형이다.
+		*/
+		
+
+		
+	}
+
+}
